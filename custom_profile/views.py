abbf0e84620e02d3fe51630e4925491cb73a2670
Mikhail Miroshnikov <mirrosha26@gmail.com>
2024-07-16 22:45:38 +0300

    Update SMTP

--- custom_profile/views.py
+++ custom_profile/views.py
@@ -104,6 +104,7 @@ def update_user_status(request):
 class CustomPasswordResetView(auth_views.PasswordResetView):
     template_name = 'registration/password_reset.html'
     email_template_name = 'registration/password_reset_email.html'
+    subject_template_name = 'registration/password_reset_subject.txt'
     success_url = reverse_lazy('password_reset_done')
 
 class CustomPasswordResetDoneView(auth_views.PasswordResetDoneView):
