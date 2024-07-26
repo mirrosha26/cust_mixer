7a02cb96f826d6586393a6b6cde115474832252d
Mikhail Miroshnikov <mirrosha26@gmail.com>
2024-07-26 12:56:28 +0300

    Update admin

--- custom_profile/admin.py
+++ custom_profile/admin.py
@@ -1,5 +1,3 @@
-# в файле admin.py вашего приложения
-
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
@@ -15,17 +13,18 @@ class CustomUserAdmin(BaseUserAdmin):
             'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
         }),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        (_('Additional info'), {'fields': ('is_connected', 'order_id', 'body_html', 'head_html')}),
     )
     
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'email', 'password1', 'password2'),
+            'fields': ('username', 'email', 'password1', 'password2', 'is_connected'),
         }),
     )
 
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'uuid', 'is_connected', 'order_id')
-    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups', 'is_connected')
     search_fields = ('username', 'first_name', 'last_name', 'email')
     ordering = ('username',)
 
